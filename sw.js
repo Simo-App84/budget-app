97263e530c90b08472b23e88a2acccc72f373c01
Simo-App84 <fsimo.84@gmail.com>
2026-05-07 13:51:31 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,8 @@
-const CACHE_NAME = 'budget-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
+const CACHE_NAME = 'budget-v2';
+const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
